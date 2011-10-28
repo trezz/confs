d45e600329221e71d678105a0b52db5b7136ccd0
Vincent Camus <vincent.camus@pulse-ar.com>
2011-10-28 11:05:34 +0200

    Adding emacs configuration files

---
+++ emacs.d/gnus.el
@@ -0,0 +1,8 @@
+(setq gnus-select-method '(nntp "news.epita.fr"))
+(setq user-mail-address "camus.vincent@gmail.com")
+(setq user-full-name "Trezz")
+(setq
+ gnus-novice-user 't
+ gnus-expert-user nil
+ gnus-signature-limit '(4.0 "----* Forwarded article")
+)
